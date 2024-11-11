f51c0fb4014fc765bd85e7fa8514b4f50ba5aef2
Jean Carlos David <jeancarlosdavidrivasraga@gmail.com>
2024-11-10 21:10:09 -0500

    update 2.0.8

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+botones = document.querySelectorAll('button');
 
 
-    // document.getElementById('alertModal').addEventListener('click', function (event) {
-    //     event.preventDefault();
-    //     var alertModal = new bootstrap.Modal(document.getElementById('alertModal')); alertModal.show();
-    // });
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        openModal(boton)
+    })
+});
 
-    document.getElementById('closeModal').addEventListener('click', function () {
+
+document.querySelectorAll('.nav-link').forEach(function (link) {
+    link.addEventListener('click', function () {
+        var offcanvas = bootstrap.Offcanvas.getInstance(document.getElementById('offcanvasNavbarDark'));
+        offcanvas.hide();
+    });
+});
+
+
+
+function openModal(boton) {
+    if (boton.id === 'closeModalAlert') { //si el id pertenece a el modal mostrar el login
         var alertModal = bootstrap.Modal.getInstance(document.getElementById('alertModal')); alertModal.hide();
         var mainModal = new bootstrap.Modal(document.getElementById('signIn')); mainModal.show();
     }
-
-    )
-
+}
