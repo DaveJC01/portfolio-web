4c0bc0dbc4e0a06ed699a2df21859b112e80a802
Jean Carlos David <jeancarlosdavidrivasraga@gmail.com>
2024-11-11 16:54:56 -0500

    update 2.0.9

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-botones = document.querySelectorAll('button');
-
-
+var botones = document.querySelectorAll('button');
 botones.forEach(boton => {
     boton.addEventListener("click", () => {
         openModal(boton)
@@ -15,11 +13,35 @@ document.querySelectorAll('.nav-link').forEach(function (link) {
     });
 });
 
-
-
 function openModal(boton) {
     if (boton.id === 'closeModalAlert') { //si el id pertenece a el modal mostrar el login
         var alertModal = bootstrap.Modal.getInstance(document.getElementById('alertModal')); alertModal.hide();
         var mainModal = new bootstrap.Modal(document.getElementById('signIn')); mainModal.show();
+    } else if (boton.id === 'btnIniciarSesion') {
+         //autenticacion de usuario
+        window.location.href = 'dashboard.html';
     }
+
 }
+
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+
+
