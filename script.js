b6aae85e602d28f9254ff509faf21a8500f154c1
Jean Carlos David <jeancarlosdavidrivasraga@gmail.com>
2024-11-14 21:11:55 -0500

    update 2.1.1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var botones = document.querySelectorAll('button');
+var botones = document.querySelectorAll('div');
 botones.forEach(boton => {
     boton.addEventListener("click", () => {
         openModal(boton)
@@ -13,35 +13,16 @@ document.querySelectorAll('.nav-link').forEach(function (link) {
     });
 });
 
-function openModal(boton) {
-    if (boton.id === 'closeModalAlert') { //si el id pertenece a el modal mostrar el login
-        var alertModal = bootstrap.Modal.getInstance(document.getElementById('alertModal')); alertModal.hide();
-        var mainModal = new bootstrap.Modal(document.getElementById('signIn')); mainModal.show();
-    } else if (boton.id === 'btnIniciarSesion') {
-         //autenticacion de usuario
-        window.location.href = 'dashboard.html';
+function openModal(div) {
+   
+    if (div.className === 'youtube') {
+        window.location.href = 'https://davejc01.github.io/The-facebook/'
     }
+    
 
 }
 
-(() => {
-    'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-
-            form.classList.add('was-validated')
-        }, false)
-    })
-})()
 
 
 
